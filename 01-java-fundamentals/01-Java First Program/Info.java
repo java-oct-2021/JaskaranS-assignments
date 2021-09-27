8686b5a99745c56174cecfdb51405d464d9988d4
Jaskaran Sahota <karansahota18@gmail.com>
2021-09-26 17:42:21 -0700

    First Commit

---
+++ 01-java-fundamentals/01-Java First Program/Info.java
@@ -0,0 +1,9 @@
+public class Info
+{
+    public static void main(String args[])
+    {
+        System.out.println("My name is Jaskaran Sahota");
+        System.out.println("I am 26 years Old");
+        System.out.println("My hometown is Seattle,Washington");
+    }
+}
